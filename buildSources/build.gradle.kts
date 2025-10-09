b78fb0d8c6da9f5be9151658049a1fadb080f360
Nikita Katkov <nikita.katkov@jetbrains.com>
2025-10-09 15:14:25 +0200

    use extracted libs

--- buildSources/build.gradle.kts
+++ buildSources/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.intellij.platform:intellij-platform-gradle-plugin:2.6.0")
-    implementation("rpc:rpc.gradle.plugin:2.1.10-0.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
-    implementation("org.jetbrains.kotlin:kotlin-serialization:2.1.10")
+    implementation(libs.intellij.platform.gradle.plugin)
+    implementation(libs.rpc.gradle.plugin)
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.serialization)
 }
